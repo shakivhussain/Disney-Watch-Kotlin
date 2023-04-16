5e3f997384f785fadd1a803c48d01561964a7025
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-04-16 21:04:40 +0530

    💻 Code Cleanup

--- app/src/main/java/com/shakiv/husain/disneywatch/data/model/details/ProductionCompany.kt
+++
@@ -1,8 +0,0 @@
-package com.shakiv.husain.disneywatch.data.model.details
-
-data class ProductionCompany(
-    val id: Int,
-    val logo_path: String,
-    val name: String,
-    val origin_country: String
-)
--- app/src/main/java/com/shakiv/husain/disneywatch/data/model/details/ProductionCountry.kt
+++
@@ -1,6 +0,0 @@
-package com.shakiv.husain.disneywatch.data.model.details
-
-data class ProductionCountry(
-    val iso_3166_1: String,
-    val name: String
-)
